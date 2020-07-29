8333f851525dc4cc5934a05252378a96a2fea594
hanfanw <hanfanw@gmail.com>
2020-07-29 12:41:31 -0400

    MEAN project

---
+++ Week03/demo/public/controllers/controller.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: Hanfan Wang
+ * @Date: 2020-07-29 11:08:16
+ * @LastEditTime: 2020-07-29 12:36:30
+ */
+function AppCtrl($scope, $http) {
+  console.log("Hello world from controller");
+
+  var refresh = function () {
+    $http.get("/contactlist").success(function (response) {
+      console.log("I got the data I requested");
+      // console.log(response);
+      $scope.contactlist = response;
+      $scope.contact = "";
+    });
+  };
+  refresh();
+  $scope.addContact = function () {
+    $http.post("/contactlist", $scope.contact).success(function (response) {
+      refresh();
+    });
+  };
+
+  $scope.remove = function (id) {
+    console.log(id);
+    $http.delete("/contactlist/" + id).success(function (response) {
+      refresh();
+    });
+  };
+
+  $scope.edit = function (id) {
+    console.log(id);
+    $http.get("/contactlist/" + id).success(function (response) {
+      $scope.contact = response;
+    });
+  };
+
+  $scope.update = function () {
+    $http
+      .put("/contactlist/" + $scope.contact._id, $scope.contact)
+      .success(function (response) {
+        refresh();
+      });
+  };
+}
--- Week03/demo/server.js
+++ Week03/demo/server.js
@@ -1,13 +1,74 @@
 /*
  * @Author: Hanfan Wang
  * @Date: 2020-07-29 10:29:36
- * @LastEditTime: 2020-07-29 10:33:17
+ * @LastEditTime: 2020-07-29 12:35:36
  */
-var express = require("express");
-var app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World from Server.js");
+// express web server
+
+var express = require("express");
+var mongojs = require("mongojs");
+var db = mongojs("contactlist", ["contactlist"]);
+
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World from Server.js");
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/contactlist", function (req, res) {
+  console.log("I received a GET request");
+
+  db.contactlist.find(function (err, docs) {
+    // console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post("/contactlist", function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete("/contactlist/:id", function (req, res) {
+  var id = req.params.id;
+  db.contactlist.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get("/contactlist/:id", function (req, res) {
+  var id = req.params.id;
+  db.contactlist.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put("/contactlist/:id", function (req, res) {
+  var id = req.params.id;
+  db.contactlist.findAndModify(
+    {
+      query: { _id: mongojs.ObjectId(id) },
+      update: {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          number: req.body.number,
+        },
+      },
+      new: true,
+    },
+    function (err, doc) {
+      res.json(doc);
+    }
+  );
 });
 app.listen(3000);
 console.log("Server is running on port 3000");
