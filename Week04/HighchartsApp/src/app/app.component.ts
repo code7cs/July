72e977bbe9e060b4187877222f4fc1c0b302dae6
hanfanw <hanfanw@gmail.com>
2020-08-04 12:10:46 -0400

    23

--- Week04/HighchartsApp/src/app/app.component.ts
+++ Week04/HighchartsApp/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+/*
+ * @Author: Hanfan Wang
+ * @Date: 2020-08-04 11:44:11
+ * @LastEditTime: 2020-08-04 12:05:34
+ */
+import { Component } from "@angular/core";
+import * as Highcharts from "highcharts";
+import { Chart } from "angular-highcharts";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'HighchartsApp';
+  title = "HighchartsApp";
+
+  chart = new Chart({
+    chart: {
+      plotBorderWidth: null,
+      plotShadow: false,
+    },
+    title: {
+      text: "Browser market shares at a specific website, 2020",
+    },
+    tooltip: {
+      pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+    },
+    plotOptions: {
+      pie: {
+        shadow: false,
+        center: ["50%", "50%"],
+        size: "45%",
+        innerSize: "20%",
+      },
+    },
+    series: [
+      {
+        type: "pie",
+        name: "Browser share",
+        data: [
+          ["Firefox", 45.0],
+          ["IE", 26.8],
+          {
+            name: "Chrome",
+            y: 12.8,
+            sliced: true,
+            selected: true,
+          },
+          ["Safari", 8.5],
+          ["Opera", 6.2],
+          ["Others", 0.7],
+        ],
+      },
+    ],
+  });
 }
--- Week04/HighchartsApp/src/app/app.module.ts
+++ Week04/HighchartsApp/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/*
+ * @Author: Hanfan Wang
+ * @Date: 2020-08-04 11:44:11
+ * @LastEditTime: 2020-08-04 12:07:53
+ */
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ChartModule } from "angular-highcharts";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ChartModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
