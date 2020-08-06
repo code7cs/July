adcaae4eb62ce3cf04a57078c4808317b7670730
hanfanw <hanfanw@gmail.com>
2020-08-06 10:02:19 -0400

    gulp

---
+++ Week04/GRUNT_JULY/Gruntfile.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Hanfan Wang
+ * @Date: 2020-08-06 09:58:28
+ * @LastEditTime: 2020-08-06 10:02:06
+ */
+// configure our tasks
+// Load the plugins
+// Register the tasks
--- Week04/Gulp_1/gulpfile.js
+++ Week04/Gulp_1/gulpfile.js
@@ -1,11 +1,12 @@
 /*
  * @Author: Hanfan Wang
  * @Date: 2020-08-06 09:23:10
- * @LastEditTime: 2020-08-06 09:26:08
+ * @LastEditTime: 2020-08-06 09:52:00
  */
 var gulp = require("gulp");
 var cssmin = require("gulp-cssmin");
 var rename = require("gulp-rename");
+var jshint = require("gulp-jshint");
 
 gulp.task("default", function (done) {
   gulp
@@ -14,4 +15,21 @@ gulp.task("default", function (done) {
     .pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest("dist"));
   done();
+});
+
+gulp.task("jshint", function (done) {
+  gulp.src("src/main.js").pipe(jshint()).pipe(jshint.reporter("default"));
+  done();
+});
+
+gulp.task("web", function () {
+  return gulp
+    .src("src/main.js")
+    .pipe(jshint())
+    .pipe(
+      jshint.reporter("gulp-jshint-html-reporter", {
+        filename: __dirname + "/jshint-output.html",
+        createMissingFolders: false,
+      })
+    );
 });
