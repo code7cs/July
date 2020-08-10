4a173b150861247595cf7b9614e494b61c971402
hanfanw <hanfanw@gmail.com>
2020-08-10 10:32:23 -0400

    update

--- Week04/Angular8_JULY/routing-app/src/app/app-routing.module.ts
+++ Week04/Angular8_JULY/routing-app/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {DepartmentListComponent} from "./department-list/department-list.component";
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 
 const routes: Routes = [
+  { path: ' ', redirectTo:'/departments', pathMatch: 'full'},
   { path: 'departments', component: DepartmentListComponent},
-  { path: 'employees', component: EmployeeListComponent}
+  { path: 'employees', component: EmployeeListComponent},
+  { path:'**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -16,4 +19,4 @@ const routes: Routes = [
 export class AppRoutingModule {
 }
 
-export const routingComponents = [DepartmentListComponent, EmployeeListComponent ];
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent ];
