a8aedc6a67727afb2870cd6f3048e04e03346465
hanfanw <hanfanw@gmail.com>
2020-08-17 19:55:04 -0400

    53

--- Week06/react_July/react-demo/src/App.js
+++ Week06/react_July/react-demo/src/App.js
@@ -6,15 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Hello name="Bruce" heroName="Batman">
+        <Hello name="Bruce" heroName="Batman">
           <p>This is from the first children prop</p>
           <p>This is from the second children prop</p>
         </Hello>
         <Hello name="Diana" heroName="Wonder Women">
           <button>CLICK</button>
         </Hello>
-        <Hello name="Clark" heroName="SuperMan" /> */}
-        <EventBind></EventBind>
+        <Hello name="Clark" heroName="SuperMan" />
+        {/* <EventBind></EventBind> */}
       </div>
     );
   }
--- Week06/react_July/react-demo/src/components/Hello.js
+++ Week06/react_July/react-demo/src/components/Hello.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 class Hello extends Component {
   render() {
+    console.log(this.props.children);
     return (
       <div>
         <h1>
           Greeting {this.props.name} a.k.a {this.props.heroName}
         </h1>
-        {this.props.children}
+        {this.props.children && this.props.children[0]}
       </div>
     );
   }
