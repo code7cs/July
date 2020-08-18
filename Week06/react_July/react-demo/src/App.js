c55a0719d7a06dd160e4d1596a7e2f27709c85ed
hanfanw <hanfanw@gmail.com>
2020-08-17 20:01:15 -0400

    54

--- Week06/react_July/react-demo/src/App.js
+++ Week06/react_July/react-demo/src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import Hello from "./components/Hello";
 import EventBind from "./components/EventBind";
+
+import styles from "./appStyles.module.css";
+import "./appStyles.css";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Hello name="Bruce" heroName="Batman">
+        <h1 className="success">SUCCESS</h1>
+        <h1 className={styles.error}>ERROR</h1>
+        {/* <Hello name="Bruce" heroName="Batman">
           <p>This is from the first children prop</p>
           <p>This is from the second children prop</p>
         </Hello>
         <Hello name="Diana" heroName="Wonder Women">
           <button>CLICK</button>
         </Hello>
-        <Hello name="Clark" heroName="SuperMan" />
+        <Hello name="Clark" heroName="SuperMan" /> */}
         {/* <EventBind></EventBind> */}
       </div>
     );
