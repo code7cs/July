91d5730b8f54827e01b6d2b92fed3e6da16ccf7a
hanfanw <hanfanw@gmail.com>
2020-08-20 00:30:46 -0400

    2

--- Week06/angular9-firebase-chat-app/src/app/app.component.ts
+++ Week06/angular9-firebase-chat-app/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'YOUR_API_KEY',
-  databaseURL: 'YOUR_DATABASE_URL'
+  apiKey: 'AIzaSyB0P6jg-85w6G42ENaYgJ9jaIR3rJWzfe4',
+  databaseURL: 'https://chat-app-93594.firebaseio.com',
 };
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-chat';
