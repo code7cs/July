a80c6c0dccb4d0d0504a536f5fee81dc890933e5
hanfanw <hanfanw@gmail.com>
2020-08-26 11:59:35 -0400

    d3

---
+++ Week07/d3demo1/src/Char1.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+
+export class Char1 extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Char1;
