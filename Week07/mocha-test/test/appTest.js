f5384793c1fa542ffa2fe20e8b0618aaba7735bc
hanfanw <hanfanw@gmail.com>
2020-08-28 18:57:39 -0400

    mocha chai

---
+++ Week07/mocha-test/test/appTest.js
@@ -0,0 +1,31 @@
+const assert = require("chai").assert;
+const app = require("../app");
+const { sayHello, addNumbers } = require("../app");
+
+// const sayHello = require("../app").sayHello;
+// const addNumbers = require("../app").addNumbers;
+
+// Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
+
+describe("App", function () {
+  describe("sayHello()", function () {
+    it("sayHello should return hello", function () {
+      assert.equal(sayHelloResult, "hello");
+    });
+
+    it("sayHello should return type string", function () {
+      assert.typeOf(sayHelloResult, "string");
+    });
+  });
+  describe("addNumbers()", () => {
+    it("addNumbers should be above 5", function () {
+      assert.isAbove(addNumbersResult, 5);
+    });
+
+    it("addNumbers should return type number", function () {
+      assert.typeOf(addNumbersResult, "number");
+    });
+  });
+});
