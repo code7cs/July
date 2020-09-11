fb976cef1838a167b1a31e65b46cb28c3207077a
Hanfan <hanfan@Hanfans-MacBook-Pro-16.local>
2020-09-11 02:38:49 -0400

    @viewChild

--- Week09/ng-demo/src/app/app.component.ts
+++ Week09/ng-demo/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ViewChildCompComponent } from './view-child-comp/view-child-comp.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ng-demo';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(ViewChildCompComponent)
+  firstMessageComponent: ViewChildCompComponent;
+
+  messages: string[] = ['Message 1', 'Message 2', 'Message 3', 'Message 4'];
+  // title = 'ng-demo';
+
+  ngAfterViewInit() {
+    this.firstMessageComponent.message =
+      'this message changed from parent component';
+  }
 }
